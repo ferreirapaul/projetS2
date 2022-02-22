15aa85c6ee64a4745c1414b227743b2a59b2888e
Paul FERRREIRA <paul.ferreira@epita.fr>
2022-02-22 18:09:53 +0100

    ajout client2

---
+++ ProjetS2/Assets/Scripts/Network/Client.cs
@@ -0,0 +1,157 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using System;
+
+
+namespace client
+{
+    public class Client : MonoBehaviour
+    {
+        public static Client instance;
+        public static int dataBufferSize = 4096;
+
+        public string ip = "127.0.0.1";
+        public int port = 26950;
+        public int myId = 0;
+
+        public TcpClient socket;
+        private delegate void PacketHandler(Packet _packet);
+        private static Dictionary<int, PacketHandler> packetHandlers;
+
+        private NetworkStream stream;
+        private Packet receivedData;
+        private byte[] receiveBuffer;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+
+        public void ConnectToServer()
+        {
+            InitializeClientData();
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+        }
+
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+
+            if (!socket.Connected)
+            {
+                return;
+            }
+
+            stream = socket.GetStream();
+
+            receivedData = new Packet();
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        private void InitializeClientData()
+        {
+            packetHandlers = new Dictionary<int, PacketHandler>()
+            {
+                { (int)IdMsg.welcome, ClientHandle.Welcome }
+            };
+            Debug.Log("Initialized packets.");
+        }
+
+        public void SendData(Packet _packet)
+        {
+            try
+            {
+                if (socket != null)
+                {
+                    stream.BeginWrite(_packet.buffer.ToArray(), 0, _packet.buffer.Count, null, null);
+                }
+            }
+            catch (Exception _ex)
+            {
+                Debug.Log($"Error sending data to server via TCP: {_ex}");
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength <= 0)
+                {
+                    // TODO: disconnect
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                receivedData.Reset(HandleData(_data));
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch
+            {
+                // TODO: disconnect
+            }
+        }
+
+        private bool HandleData(byte[] _data)
+        {
+            int _packetLength = 0;
+
+            receivedData.SetBytes(_data);
+
+            if (receivedData.UnreadLength() >= 4)
+            {
+                _packetLength = receivedData.ReadInt();
+                if (_packetLength <= 0)
+                {
+                    return true;
+                }
+            }
+
+            while (_packetLength > 0 && _packetLength <= receivedData.UnreadLength())
+            {
+                byte[] _packetBytes = receivedData.ReadBytes(_packetLength);
+                ThreadManager.ExecuteOnMainThread(() =>
+                {
+                    using (Packet _packet = new Packet(_packetBytes))
+                    {
+                        int _packetId = _packet.ReadInt();
+                        packetHandlers[_packetId](_packet);
+                    }
+                });
+
+                _packetLength = 0;
+                if (receivedData.UnreadLength() >= 4)
+                {
+                    _packetLength = receivedData.ReadInt();
+                    if (_packetLength <= 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            if (_packetLength <= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ ProjetS2/Assets/Scripts/Network/ClientSend.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace client
+{
+    public class ClientSend : MonoBehaviour
+    {
+        private static void SendTCPData(Packet _packet)
+        {
+            _packet.Write(_packet.buffer.Count);
+            Client.instance.SendData(_packet);
+        }
+    }
+}
