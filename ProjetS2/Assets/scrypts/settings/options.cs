2ccfdb3206558e2ef1e4f0b10ff9e8de955adea6
albaty <plavielle66@gmail.com>
2022-03-04 16:33:44 +0100

    le "merge" de l'UI

---
+++ ProjetS2/Assets/scrypts/settings/options.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class options : MonoBehaviour
+{
+    public GameObject Panel;
+    bool visible = false;
+
+    public Dropdown DResolution;
+
+    public AudioSource audiosource;
+    public Slider slider;
+    public Text TxtVolume;
+
+    public GameObject KeyBinding;
+
+    private void Start() 
+    {
+        SliderChange();
+        KeyBinding.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            visible = !visible;
+            Panel.SetActive(visible);
+        }
+
+    }
+
+    public void SetResolution()
+    {
+        switch (DResolution.value)
+        {
+            case 0:
+                Screen.SetResolution(640, 360, true);
+                break;
+
+            case 1:
+                Screen.SetResolution(1600, 900, true);
+                break;
+
+            case 2:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+        }
+    }
+
+    public void SliderChange()
+    {
+        audiosource.volume = slider.value;
+        TxtVolume.text = "Volume " + (audiosource.volume * 100).ToString("00") + "%";
+    }
+}
