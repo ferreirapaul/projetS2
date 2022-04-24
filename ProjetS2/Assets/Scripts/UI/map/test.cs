35932cd1143ae19092e3b40befa6ccc3267b4507
albaty <plavielle66@gmail.com>
2022-04-24 17:22:53 +0200

    zoom ready but can optimize

---
+++ ProjetS2/Assets/Scripts/UI/map/test.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Camera cam;
+    private Vector3 drag;
+    public float zoom, minsize, maxsize;
+
+    public SpriteRenderer mapRenderer;
+    private float mapMinx, mapMaxx,mapMiny,mapMaxy;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mapMinx = mapRenderer.transform.position.x - mapRenderer.bounds.size.x/2f;
+        mapMaxx = mapRenderer.transform.position.x + mapRenderer.bounds.size.x / 2f;
+
+        mapMiny = mapRenderer.transform.position.y - mapRenderer.bounds.size.y / 2f;
+        mapMaxy = mapRenderer.transform.position.y + mapRenderer.bounds.size.y / 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PanCamera();
+    }
+
+    private void PanCamera()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            drag = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 diff =drag- cam.ScreenToWorldPoint(Input.mousePosition);
+            cam.transform.position = Cameramov(cam.transform.position+diff);
+        }
+        
+    }
+    public void ZoomIn()
+    {
+
+        float newSize = cam.orthographicSize + zoom;
+        cam.orthographicSize = Mathf.Clamp(newSize, minsize, maxsize);
+        cam.transform.position = Cameramov(cam.transform.position);
+    }
+    public void Zoomout()
+    {
+
+        float newSize = cam.orthographicSize - zoom;
+        cam.orthographicSize = Mathf.Clamp(newSize, minsize, maxsize);
+        cam.transform.position = Cameramov(cam.transform.position);
+    }
+    private Vector3 Cameramov(Vector3 targetp) => new Vector3(Mathf.Clamp(targetp.x,
+                                                                         mapMinx + (cam.orthographicSize * cam.aspect),
+                                                                         mapMaxx - (cam.orthographicSize * cam.aspect))
+                                                              , Mathf.Clamp(targetp.y,
+                                                                           mapMiny + cam.orthographicSize,
+                                                                           mapMaxy - cam.orthographicSize)
+                                                              , targetp.z);
+}
---
+++ Reseaux/Server/Server/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] arg)
+    {
+        Server.Server.Start();
+        Console.ReadLine();
+    }
+}
